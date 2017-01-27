7ccfeb3ae461240c1b5cd6c87224978319dc3e67
VUTSAL <VUTSAL@GMAIL.COM>
2017-01-27 00:54:08 -0800

    3rd party library nodemailer added.Code to send mail added in forgot password api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var nodemailer = require('nodemailer');
 
 app.use(express.static(__dirname + "/public"));
 
@@ -23,12 +23,46 @@ app.post('/CheckregisterUser',function(req,res) {
 //-------------------------------Services for Login page-------------------------------//
 app.post('/forgotPassword',function(req,res) {
 	
-	if (req.param('email')=="vatsal@gmail.com") 
-		{res.json("Valid User");
-	//ToDo:Code to email the password 
+	if (req.param('email')=="djethwa2810@gmail.com") 
+		{
+
+			res.json("Valid User");
+
+	//ToDo:Code to email the password
+	  var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+                auth: {
+            user: 'brogrammerrs@gmail.com', // Your email id
+            pass: '2541temple' // Your password
+        }
+    });
+	  var mailOptions = {
+    from: 'brogrammerrs@gmail.com', // sender address
+    to: req.param('email'), // list of receivers
+    subject: 'Recover Password', // Subject line
+    text: "Your password is admin" // plaintext body
+    
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+        res.json({yo: 'error'});
+    }else{
+        console.log('Message sent: ' + info.response);
+        res.json({yo: info.response});
+    };
+});
 	}
 		else{ res.json("Invalid User");}
 });
+
+
+
+ //-------------------------------Services for Login page-------------------------------//
+
 
 app.listen(3000);
 console.log("server running on 3000");
