c63795f46b38db70ca812abf427cd9ef2327b687
tarun20mogra <tarun20mogra@gmail.com>
2017-03-05 18:35:31 -0800

    mongo connection on ec2

--- database/mlab_connection.js
+++ database/mlab_connection.js
@@ -8,11 +8,18 @@ var express = require('express');
 var app = express();
 var db ;
 
-    MongoClient.connect('mongodb://admin:root@ds153669.mlab.com:53669/quick_rent_database',function (err, database) {
-        if (err) return console.log(err)
-        db = database
-        app.listen(3000, function() {
-            console.log('listening on 3000')
-        })
-    }) ;
+    // MongoClient.connect('mongodb://admin:root@ds153669.mlab.com:53669/quick_rent_database',function (err, database) {
+    //     if (err) return console.log(err)
+    //     db = database
+    //     app.listen(3000, function() {
+    //         console.log('listening on 3000')
+    //     })
+    // }) ;
+MongoClient.connect('mongodb://admin:root@ec2-52-32-216-134.us-west-2.compute.amazonaws.com:27017/admin',function (err, database) {
+    if (err) return console.log(err)
+    db = database
+    app.listen(3000, function() {
+        console.log('listening on 3000')
+    })
+}) ;
 
